42e6cb7ff5b2768434b714db1beb875cf2e82ad3
smlsunxie <smlsun@gmail.com>
2015-08-02 21:49:24 +0800

    add shop link

--- server.js
+++ server.js
@@ -3,24 +3,33 @@ var router = require('koa-router')();
 var request = require("co-request");
 var mount = require('koa-mount');
 var path = require('path');
-
+var staticCache = require('koa-static-cache');
 
 var app = module.exports = koa();
 
-var addr = process.env.MOBILE_PORT_1337_TCP_ADDR || 'localhost';
-var port = process.env.MOBILE_PORT_1337_TCP_PORT || '3000';
+var addr = process.env.DOMAIN_NAME || 'localhost';
+var port = process.env.MOBILE_PORT_3000_TCP_PORT || '3000';
+
 
 var mobileServerUrl = 'http://' + addr + ':' + port;
-
+var get_ip = require('ipware')().get_ip;
+var requestIp = require('request-ip');
 
 router.get('/shop', function *(next) {
+  console.log(mobileServerUrl + '/index.html');
   this.redirect(mobileServerUrl + '/index.html')
 });
 
+router.get('/', function *(next) {
+  this.redirect('/examples/agricloud/shop.html');
+});
 
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
-app.use(serve('.'));
+app.use(mount('/', staticCache(path.join(__dirname, '.'))));
 
-var port = 3000;
+var port = 2000;
 
 if (!module.parent) app.listen(port);
